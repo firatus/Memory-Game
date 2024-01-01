79c37c2778e8bc3d4cdb2f69b692809521d499d4
Fırat Doğan <firaterdemdogan@gmail.com>
2024-01-01 15:09:54 +0300

    v2

--- Memory Game/script.js
+++ Memory Game/script.js
@@ -89,39 +89,32 @@ const matrixGenerator = (cardValues, size = 4) => {
   cards = document.querySelectorAll(".card-container");
   cards.forEach((card) => {
     card.addEventListener("click", () => {
-      //If selected card is not matched yet then only run (i.e already matched card when clicked would be ignored)
       if (!card.classList.contains("matched")) {
-        //flip the cliked card
         card.classList.add("flipped");
-        //if it is the firstcard (!firstCard since firstCard is initially false)
+
         if (!firstCard) {
-          //so current card will become firstCard
           firstCard = card;
-          //current cards value becomes firstCardValue
+
           firstCardValue = card.getAttribute("data-card-value");
         } else {
-          //increment moves since user selected second card
           moves_counter();
-          //secondCard and value
+
           secondCard = card;
           let secondCardValue = card.getAttribute("data-card-value");
           if (firstCardValue == secondCardValue) {
-            //if both cards match add matched class so these cards would beignored next time
             firstCard.classList.add("matched");
             secondCard.classList.add("matched");
-            //set firstCard to false since next card would be first now
+
             firstCard = false;
-            //winCount increment as user found a correct match
+
             winCount += 1;
-            //check if winCount ==half of cardValues
+
             if (winCount == Math.floor(cardValues.length / 2)) {
               result.innerHTML = `<h2>You Won</h2>
             <h4>Moves: ${movesCount}</h4>`;
               stopGame();
             }
           } else {
-            //if the cards dont match
-            //flip the cards back to normal
             let [tempFirst, tempSecond] = [firstCard, secondCard];
             firstCard = false;
             secondCard = false;
@@ -141,13 +134,13 @@ start_btn.addEventListener("click", () => {
   movesCount = 0;
   second = 0;
   minute = 0;
-  //controls amd buttons visibility
+
   controls.classList.add("hide");
   stop_btn.classList.remove("hide");
   start_btn.classList.add("hide");
-  //Start timer
+
   interval = setInterval(time_generator, 1000);
-  //initial moves
+
   moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
   initializer();
 });
